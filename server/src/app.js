fd9fc48e790d5f3547f924953cda3ab9ac53fcf5
manje22 <uvodicmariela@gmail.com>
2025-04-14 22:38:18 +0200

    set up project

---
+++ server/src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+
+// Middleware
+app.use(express.json());
+
+// Routes
+
+
+// Error handling middleware could go here
+
+module.exports = app;
