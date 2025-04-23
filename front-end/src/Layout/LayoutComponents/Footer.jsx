d0c338a40b1f81b3343bb18d316dbff3a790610c
manje22 <uvodicmariela@gmail.com>
2025-04-23 10:25:20 +0200

    added basic styling to navbar components

--- front-end/src/Layout/LayoutComponents/Footer.jsx
+++ front-end/src/Layout/LayoutComponents/Footer.jsx
@@ -1,11 +1,10 @@
 function MainFooter() {
     return (
-      <div className="absolute bottom-0 w-full">
-        <footer className="flex flex-row justify-around">
+      <div className="absolute left-0 bottom-0 bg-gray-100 w-full ml-0">
+        <footer className="flex flex-row justify-around gap-1.5">
           <h1>Contact info</h1>
           <div>tel: +385 098 968 3709</div>
           <div>e-mail: apartmanimariela@gmail.com</div>
-          <br />
           <div>address: Ulica domovinskog rata 74E</div>
         </footer>
       </div>
--- front-end/src/Layout/LayoutComponents/Navbar.jsx
+++ front-end/src/Layout/LayoutComponents/Navbar.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router";
 
 function NavBar() {
     return(
-        <div className="w-full">
-            <nav className="flex flex-row w-full justify-around">
-                <p>Contact us</p>
+        <div className="border-b-1 border-gray-200">
+            <nav className="sticky inset-x-0 flex flex-row top-0 justify-between  p-5">
+                <Link to={'/information'}>Contact us</Link>
                 <Link to={'/login'}>Log in</Link>
                 <Link to={'/registration'}>Register</Link>
                 <Link to={'/gallery'}>Gallery</Link>
