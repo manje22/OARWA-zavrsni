36137d27d8b42a01841428f517746f815b7e4ecc
manje22 <uvodicmariela@gmail.com>
2025-04-25 14:40:35 +0200

    added registration auth service

--- front-end/src/services/AuthServices.js
+++ front-end/src/services/AuthServices.js
@@ -5,3 +5,9 @@ export const loginUser = async (data) => {
   console.log(res);
   return res;
 };
+
+export const registerUser = async (data) => {
+  const res = await axios.post("http://localhost:3000/register", data);
+  console.log(res);
+  return res;
+}
