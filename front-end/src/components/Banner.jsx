e68224a951f3f46f55ca7f9f6ed1193b821fb6b1
manje22 <uvodicmariela@gmail.com>
2025-04-26 12:18:58 +0200

    created banner component

---
+++ front-end/src/components/Banner.jsx
@@ -0,0 +1,16 @@
+function Banner({ picSrc, title, subTitle }) {
+    return (
+      <div
+        className="w-full min-h-150 bg-cover bg-center relative"
+        style={{ backgroundImage: `url(${picSrc})` }}
+      >
+        <div className="absolute inset-0 bg-opacity-30 flex flex-col justify-center items-center text-white">
+          <h1 className="text-4xl font-bold">{title}</h1>
+          <h3 className="text-xl mt-2">{subTitle}</h3>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Banner;
+  
