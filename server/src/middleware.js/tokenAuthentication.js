f95c0566fff42b6dfcbc243948540508738fdc0e
manje22 <uvodicmariela@gmail.com>
2025-04-29 22:05:36 +0200

    modified checkToken middleware

--- server/src/middleware.js/tokenAuthentication.js
+++ server/src/middleware.js/tokenAuthentication.js
@@ -1,15 +1,15 @@
 const checkToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
-    if (!authHeader) return res.status(403).send('Ne postoji autorizacijsko zaglavlje');
+    if (!authHeader) return res.status(403).send('Authentification header does not exist');
   
     const token = authHeader.split(' ')[1];
-    if (!token) return res.status(403).send('Bearer token nije pronađen');
+    if (!token) return res.status(403).send('Bearer token not found');
   
     try {
       const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
       req.user = decodedToken;
     } catch (err) {
-      return res.status(401).send('Neispravni Token');
+      return res.status(401).send('Invalid Token');
     }
     return next();
   };
