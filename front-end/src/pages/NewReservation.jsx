9bcf3dbe2ca1f6f0ddb44426e05d029da294db9e
manje22 <uvodicmariela@gmail.com>
2025-05-05 20:47:18 +0200

    created newReservation component

--- front-end/src/pages/NewReservation.jsx
+++ front-end/src/pages/NewReservation.jsx
@@ -1,28 +1,87 @@
-import Banner from "../components/Banner"
-import MainLayout from "../Layout/MainLayout"
-import panoramaLokacija from "../assets/panoramaLokacijaPogled.jpg"
-import { useState, useContext } from "react"
-import LoginRedirect from "../components/LoginRedirect"
+import Banner from "../components/Banner";
+import MainLayout from "../Layout/MainLayout";
+import panoramaLokacija from "../assets/panoramaLokacijaPogled.jpg";
+import { useState, useContext } from "react";
+import LoginRedirect from "../components/LoginRedirect";
 import { UserContext } from "../contexts/UserContext";
+import { HandleChange } from "../utils/forms";
 
 function NewReservation(params) {
-    const {currentUser, setCurrentUser} = useContext(UserContext);
-    const [resFormData, setResFormData]= useState({
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+  const [resFormData, setResFormData] = useState({
+    checkIn: undefined,
+    checkOut: undefined,
+    numAdults: undefined,
+    numChildren: undefined,
+  });
 
-    })
+  function HandleChangeNewReservation(event) {
+    HandleChange(event, resFormData, setResFormData);
+  }
 
-    if (!currentUser) {
-        return <LoginRedirect></LoginRedirect>
-    }
+  if (!currentUser) {
+    return <LoginRedirect></LoginRedirect>;
+  }
 
-    return(
-        <MainLayout>
-            <Banner picSrc={panoramaLokacija} title={"Book your stay now"}></Banner>
+  return (
+    <MainLayout>
+      <Banner picSrc={panoramaLokacija} title={"Book your stay now"}></Banner>
+      <div>
+        <div className="bg-blue-100 rounded-4xl w-fit m-auto p-5 mt-10 mb-10">
+          <form className="flex justify-center gap-15">
+            <div>Check in:</div>
+            <div>Check out:</div>
             <div>
-                new res
+              <label htmlFor="numAdults">Adults:</label>
+              <select
+                name="numAdults"
+                value={resFormData.numAdults}
+                onChange={HandleChangeNewReservation}
+              >
+                <option>Select</option>
+                {[1, 2, 3, 4, 5, 6, 7, 8].map((num) => (
+                  <option value={num} key={num}>
+                    {num}
+                  </option>
+                ))}
+              </select>
             </div>
-        </MainLayout>
-    )
+            <div>
+              <label htmlFor="numChildren">Children:</label>
+              <select
+                name="numChildren"
+                value={resFormData.numAdults}
+                onChange={HandleChangeNewReservation}
+              >
+                <option>Select</option>
+                {[0, 1, 2, 3, 4, 5].map((num) => (
+                  <option value={num} key={num}>
+                    {num}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </form>
+          <button onClick={() => console.log(resFormData)}>
+            Print res details
+          </button>
+        </div>
+        <div>
+          <h1 className="text-4xl">Your reservation:</h1>
+          {Object.values(resFormData).every((el) => el === undefined) ? (
+            <div>Your reservation details will apear here</div>
+          ) : (
+            <div>
+              <p>Check in: {resFormData.checkIn}</p>
+              <p>Check out: {resFormData.checkOut}</p>
+              <p>Adults: {resFormData.numAdults}</p>
+              <p>Children: {resFormData.numChildren}</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </MainLayout>
+  );
 }
 
-export default NewReservation
+export default NewReservation;
