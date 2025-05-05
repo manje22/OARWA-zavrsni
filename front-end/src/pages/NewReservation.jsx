32f4124b02f2e543d11712550d5f6cc8c58c6940
manje22 <uvodicmariela@gmail.com>
2025-05-05 19:44:51 +0200

    LoginRedirect now renders in newRes page only if current user is null

--- front-end/src/pages/NewReservation.jsx
+++ front-end/src/pages/NewReservation.jsx
@@ -1,18 +1,25 @@
 import Banner from "../components/Banner"
 import MainLayout from "../Layout/MainLayout"
 import panoramaLokacija from "../assets/panoramaLokacijaPogled.jpg"
-import { useState } from "react"
+import { useState, useContext } from "react"
 import LoginRedirect from "../components/LoginRedirect"
+import { UserContext } from "../contexts/UserContext";
 
 function NewReservation(params) {
+    const {currentUser, setCurrentUser} = useContext(UserContext);
     const [resFormData, setResFormData]= useState({
 
     })
+
+    if (!currentUser) {
+        return <LoginRedirect></LoginRedirect>
+    }
+
     return(
         <MainLayout>
             <Banner picSrc={panoramaLokacija} title={"Book your stay now"}></Banner>
             <div>
-                <LoginRedirect></LoginRedirect>
+                new res
             </div>
         </MainLayout>
     )
