ba661e0acd15681d7f839f70fc9aef59a6703032
manje22 <uvodicmariela@gmail.com>
2025-05-07 18:11:48 +0200

    fixed

--- front-end/src/utils/toUTCMidnight.js
+++ front-end/src/utils/toUTCMidnight.js
@@ -1,3 +1,7 @@
-export default function toUTCMidnight(date) {
-    return new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
-  }
+const toUTCMidnight = (date)  => {
+  return new Date(
+    Date.UTC(date.getFullYear(), date.getMonth(), date.getDate())
+  );
+}
+
+export default toUTCMidnight;
