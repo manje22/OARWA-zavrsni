0a8c515727665acdc6e6dcf2f13e7b0e636352b3
manje22 <uvodicmariela@gmail.com>
2025-05-11 17:58:31 +0200

    removed delete button from AdminResView

--- front-end/src/components/AdminResView.jsx
+++ front-end/src/components/AdminResView.jsx
@@ -1,7 +1,4 @@
 function AdminResView({reservation}){
-    function HandleDelete(){
-        return;
-    }
     return(
         <div>
             <div>
@@ -10,9 +7,6 @@ function AdminResView({reservation}){
                 <div>Children:{reservation.numChildren}</div>
                 <div>Check in: {reservation.CheckIn}</div>
                 <div>Check out: {reservation.CheckOut}</div>
-            </div>
-            <div>
-                <button onClick={HandleDelete}>Delete</button>
             </div>
         </div>
     );
