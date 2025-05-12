71686b07916b5781bf695e15ae8a5da07925d877
manje22 <uvodicmariela@gmail.com>
2025-05-12 18:27:40 +0200

    added service for deletion of reservations

--- front-end/src/services/AdminServices.js
+++ front-end/src/services/AdminServices.js
@@ -1,8 +1,19 @@
 import axios from "axios";
 
 export const GetReservations = async (currentUser) => {
-    const res = await axios.post("http://localhost:3000/reservations/getReservationsAdmin", currentUser);
-    console.log(res);
-    return res
+  const res = await axios.post(
+    "http://localhost:3000/reservations/getReservationsAdmin",
+    currentUser
+  );
+  console.log(res);
+  return res.data;
 };
 
+export const DeleteReservation = async (resID) => {
+  const res = await axios.delete(
+    "http://localhost:3000/reservations/deleteReservationsAdmin",
+    currentUser
+  );
+  console.log(res);
+  return res.data;
+};
