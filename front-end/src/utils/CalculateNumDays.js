b1654d468a560ee31617b9c97332486bf24e82e5
manje22 <uvodicmariela@gmail.com>
2025-05-13 16:15:26 +0200

    debugging

--- front-end/src/utils/CalculateNumDays.js
+++ front-end/src/utils/CalculateNumDays.js
@@ -1,14 +1,12 @@
 export default function calculateNumDays(date1, date2) {
-let utc1 = 
-	Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
-let utc2 = 
-	Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
+  let utc1 = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
+  let utc2 = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
 
-// Calculate the time difference in milliseconds
-let timeDiff = Math.abs(utc2 - utc1);
+  // Calculate the time difference in milliseconds
+  let timeDiff = Math.abs(utc2 - utc1);
 
-// Convert milliseconds to days
-let daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+  // Convert milliseconds to days
+  let daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
 
-return daysDiff;
+  return daysDiff;
 }
